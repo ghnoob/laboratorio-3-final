29a0f194b79610711509a31ae3a4576be7c80994
ghnoob <ptnchk.rodrigo@gmail.com>
2021-07-21 13:25:32 -0300

    feat: Transacciones con Dai

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,7 @@ export default createStore({
       transactions: [],
       cryptoCodes: [
         { code: 'btc', name: 'Bitcoin', color: '#ff9315' },
+        { code: 'dai', name: 'Dai', color: '#fd024f' },
         { code: 'eth', name: 'Ethereum', color: '#5b73a0' },
         { code: 'usdc', name: 'USD Coin', color: '#2775ca' },
       ],
